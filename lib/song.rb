7aedcdcf11aa88a7ba86298659e9ddc2fbbba5dc
Charles Adkins <cpeadkins@gmail.com>
2020-03-26 16:07:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,38 @@
-require 'pry'
 
-class Song
-  attr_accessor :name
-  attr_reader :artist
 
-  @@songs = []
+# require 'pry'
 
-  def initialize
-    @@songs << self
-  end
+# class Song
+#   attr_accessor :name
+#   attr_reader :artist
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+#   @@songs = []
 
-  def self.all
-    @@songs
-  end
+#   def initialize
+#     @@songs << self
+#   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
+#   def self.find_by_name(name)
+#     @@songs.detect{|a| a.name == name}
+#   end
 
-  # def self.count
-  #   self.all.count
-  # end
+#   def self.all
+#     @@songs
+#   end
 
-  def artist=(artist)
-    @artist = artist
-  end
+#   def self.reset_all
+#     self.all.clear
+#   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+#   def self.count
+#     self.all.count
+#   end
+
+#   def artist=(artist)
+#     @artist = artist
+#   end
+
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
