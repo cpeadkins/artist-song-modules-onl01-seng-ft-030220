531c23ec1b3a127a4b768d06bbdc539358964d5c
Charles Adkins <cpeadkins@gmail.com>
2020-03-26 16:07:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,34 @@
+require 'pry'
 
+class Song
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods 
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def initialize
+    self.class.all << self
+  end
+
+  def self.all
+   @@songs
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+end
 
 # require 'pry'
 
